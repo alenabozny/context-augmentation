4faaf77915f8239888e87fa5922f2e1ade12eb01
bbalcerzak <bartomiej.balcerzak@gmail.com>
2019-03-06 12:54:38 +0100

    text rank implementation

---
+++ text_rank.py
@@ -0,0 +1,73 @@
+#TODO
+#Tokenizacja dla Polskiego
+#output, ile najbardziej kluczowych zdań wybrać?
+#refactoring do igraph
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine
+import numpy as np
+import networkx as nx
+VectorMaker = TfidfVectorizer()
+
+#This function reads the file from a given location
+def get_file(src_file):
+    file_open = open(src_file, 'r', encoding='utf-8')
+    file_read = file_open.read()
+    file_read = file_read.lower()
+    return file_read
+
+#This function takes a given text, in which each line represents a single sentence
+#A TFIDF transformation is done, and the output tfidf matrix is returned as a dense matrix (both the matrix and its transposition
+def get_tfidf(doc):
+    sentence_list = doc.split('\n')
+    tfidf = VectorMaker.fit_transform(sentence_list)
+    dense_tfidf = tfidf.todense()
+    dense_tfidf = np.asarray(dense_tfidf)
+    t_dense = dense_tfidf.transpose()
+    return dense_tfidf, t_dense
+
+#Function for calculating the sentence ranks
+def get_ranks(mat, t_mat):
+    dims = mat.shape
+
+    similarities = np.zeros((dims[0], dims[0]))
+    #calculate cosine similarities matrix between sentences
+    for d in range(dims[0]):
+        for e in range(dims[0]):
+            similarities[d, e] = (1 - cosine(mat[d, :], t_mat[:, e]))
+    #build graph for rank extraction
+    graph = nx.from_numpy_matrix(similarities)
+    #apply pagerank algorithm
+    rank = nx.pagerank(graph)
+    return rank
+
+#extract a list of top most ranked sentences from a document
+def get_ranked(ranks, sent_list, top_rank):
+    x = ranks.values()
+    x = list(x)
+    x.sort(reverse=True)
+    top = x[:top_rank]
+    selected_ranks = []
+    for key in ranks:
+        for t in top:
+            if ranks[key] == t:
+                selected_ranks.append(key)
+    text = [sent_list[t-1] for t in selected_ranks]
+    results = [t[1].capitalize() + t[2:] for t in text]
+    return results
+
+#pipeline function
+def main(file_name):
+    intake = get_file(file_name)
+    sents = get_tfidf(intake)
+    current_ranks = get_ranks(sents[0], sents[1])
+    return current_ranks
+
+
+print(get_ranked(main("C:/Users/barto/Desktop/1.txt"), get_file("C:/Users/barto/Desktop/1.txt").split('\n'), 3))
+
+
+
+
+
+
+
